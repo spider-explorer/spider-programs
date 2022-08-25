cfeea1a0d34d538ec292f961dc2fa39163537d94
JavaCommons Technologies <javacommons@gmail.com>
2022-08-25 17:49:19 +0900

    .

--- investigate.js
+++ investigate.js
@@ -2,7 +2,6 @@ async function execute(v) {
     const p = Deno.run({
         cmd: v
     });
-
     const { success, code } = await p.status();
     let result = {};
     result.success = success;
@@ -16,7 +15,6 @@ async function executePipe(v) {
         stdout: "piped",
         stderr: "piped",
     });
-
     const { success, code } = await p.status();
     let result = {};
     result.success = success;
@@ -28,30 +26,40 @@ async function executePipe(v) {
     return result;
 }
 
+async function scoopAppInfo(key, path) {
+    await execute(["cmd.exe", "/c", "scoop", "install", key]);
+    let st = await executePipe(["scoop-console-x86_64-static.exe", "--latest", key]);
+    let list = st.stdout.trim().split(" ");
+    let version = list[0];
+    let dir = list[1];
+    let url = `https://github.com/spider-explorer/spider-programs/releases/download/64bit/${key}-${version}.7z`;
+    st = await executePipe(["curl.exe", url, "-o", "/dev/null", "-w", "%{http_code}", "-s"]);
+    return { "name": key, "path": path, "version": version, "dir": dir, "url": url, "exists": st.stdout != "404" };
+}
+
 console.log("hello!");
 
 console.log(await executePipe(["echo", "hello漢字©"]));
-/*
-const p = Deno.run({
-    cmd: ["echo", "hello漢字©"],
-});
 
-// 完了を待つ
-let st = await p.status();
-console.log(st);
-*/
+let investigate = JSON.parse(await Deno.readTextFile('investigate.json'));
+console.log(investigate);
 
 let programs = JSON.parse(await Deno.readTextFile('programs.json'));
-console.log(Object.keys(programs));
+let keys = Object.keys(programs);
+console.log(keys);
 
-console.log(Deno.cwd());
+for (let key of keys)
+{
+    let app = await scoopAppInfo(key, programs[key]);
+    console.log(app);
+    investigate[app.name] = { "version": app.version, "path": app.path, "url": app.url };
+}
 
-Deno.chdir(Deno.env.get("HOME"));
+let cwd = Deno.cwd();
 
-console.log(Deno.cwd());
+Deno.writeTextFile("spider-programs.json", JSON.stringify(investigate, null, 2));
+//console.log(Deno.cwd());
 
-let st = await executePipe(["scoop-console-x86_64-static.exe", "--latest", "vscode"]);
-console.log(st);
-console.log(st.stdout.trim().split(" "));
+//Deno.chdir(Deno.env.get("HOME"));
 
-await execute(["cmd.exe", "/c", "scoop", "install", "vscode"]);
+//console.log(Deno.cwd());
