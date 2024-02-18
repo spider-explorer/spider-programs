b020175be95cfd546d26f36b1b6317a84a709300
JavaCommons Technologies <javacommons@gmail.com>
2024-02-18 13:54:04 +0900

    .

--- build.js
+++ build.js
@@ -50,6 +50,9 @@ for (var rec of programs)
     if (!app.exists)
     {
         sys.chdir(app.dir);
+        if (app.name == "git") {
+           await sys.run(["cmd.exe", "/c", "cp", cwd + "/gitconfig", app.dir + "/gitconfig"]);
+        }
         await sys.run(["cmd.exe", "/c", "dir"]);
 		if (sys.exists("IDE/bin/idea.properties")) {
            await sys.run(["sed", "-i",
